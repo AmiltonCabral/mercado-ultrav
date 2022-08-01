c402d78ad7400bbdee24581eaae62f760a270ee2
AmiltonCabral <amilton.cabral@ccc.ufcg.edu.br>
2022-08-01 20:15:52 -0300

    created head

--- pages/cart.js
+++ pages/cart.js
@@ -1,6 +1,11 @@
+import Head from "next/head"
+
 export default function Cart() {
   return (
     <>
+      <Head>
+        <title>Your planets Cart</title>
+      </Head>
       <h1>Carrinho de Compras</h1>
     </>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
+import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 //<div className={styles.container}>
 export default function Home() {
   return (
-    <div>
-      <h1>Hello Next JS</h1>
-    </div>
+    <>
+      <Head>
+        <title>Ultra V - Buy planets on sale</title>
+        <meta name="keyworkds" content="Planets, Moons, Suns"></meta>
+        <meta name="description" content="Buy planets on sale"></meta>
+      </Head>
+      <div>
+        <h1>Hello Next JS</h1>
+      </div>
+    </>
   )
 }
