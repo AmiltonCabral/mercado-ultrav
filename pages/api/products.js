af2258dc9d7731206aa913408bba645f10c93d5b
AmiltonCabral <amilton.cabral@ccc.ufcg.edu.br>
2022-08-06 18:12:22 -0300

    added jupiter

--- pages/api/products.js
+++ pages/api/products.js
@@ -13,47 +13,54 @@ export default function handler(req, res) {
         },
         {
           "id": 2,
-          "name": "Earth",
-          "price": 7500000000,
-          "image": "earth"
-        },
-        {
-          "id": 3,
-          "name": "Mars",
-          "price": 3000000000,
-          "image": "mars"
-        },
-        {
-          "id": 4,
           "name": "Mercury",
           "price": 500000,
           "image": "mercury"
         },
         {
-          "id": 5,
-          "name": "Neptune",
+          "id": 3,
+          "name": "Venus",
           "price": 500000.00,
-          "image": "neptune"
+          "image": "venus"
+        },
+        {
+          "id": 4,
+          "name": "Earth",
+          "price": 7500000000,
+          "image": "earth"
+        },
+        {
+          "id": 5,
+          "name": "Mars",
+          "price": 3000000000,
+          "image": "mars"
         },
         {
           "id": 6,
+          "name": "Jupiter",
+          "price": 3000000000,
+          "image": "jupiter"
+        },
+        {
+          "id": 7,
           "name": "Saturn",
           "price": 500000.00,
           "image": "saturn"
         },
         {
-          "id": 7,
+          "id": 8,
           "name": "Uranus",
           "price": 500000.00,
           "image": "uranus"
         },
         {
-          "id": 8,
-          "name": "Venus",
-          "price": 500000.00,
-          "image": "venus"
-        },        {
           "id": 9,
+          "name": "Neptune",
+          "price": 500000.00,
+          "image": "neptune"
+        },
+        {
+          "id": 10,
           "name": "Moon",
           "price": 500000.00,
           "image": "moon"
